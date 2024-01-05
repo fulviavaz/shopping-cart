dc31403369aab29751a03bc9d1a6f2d43ddcc0ca
fulviavaz <fulviavaz@hotmail.com>
2024-01-05 09:26:14 -0300

    adicionando produto ao carrinho

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 function adicionar() {
-    //recuperar os valores do formulário (nome,quantidade e valor)
-    let produto = document.getElementById('produto').value;
-    //separar o nome e o valor (estão juntos no mesmo id) 
-    //aqui separamos por oq esta antes e depois do ifen
-    let nomeProduto = produto.split('-')[0]; //pega oq esta na primeira posição (antes do ifen)
-    let valorUnitario = produto.split('R$')[1]; //pega oq esta na segunda posição (depois do R$)
-    let quantidade = document.getElementById('quantidade');
-    // alert(nomeProduto);
-    // alert(valorUnitario);
-    // alert(produto.value)
-    // alert(quantidade.value);
-    let preco = quantidade.value * valorUnitario; //calcula o valor total do iten
-    // alert(preco);
-
-    //calcular o preço, nossos subtotal 
-    //adicionar ao carrinho
-    //atualizar o valor total
-
+  //recuperar os valores do formulário (nome,quantidade e valor)
+  let produto = document.getElementById("produto").value;
+  //separar o nome e o valor (estão juntos no mesmo id)
+  //aqui separamos por oq esta antes e depois do ifen
+  let nomeProduto = produto.split("-")[0]; //pega oq esta na primeira posição (antes do ifen)
+  let valorUnitario = produto.split("R$")[1]; //pega oq esta na segunda posição (depois do R$)
+  let quantidade = document.getElementById("quantidade").value;
+  // alert(nomeProduto);
+  // alert(valorUnitario);
+  // alert(produto.value)
+  // alert(quantidade.value);
+  //calcular o preço subtotal
+  let preco = quantidade * valorUnitario; //calcula o valor total do iten
+  // alert(preco);
+  //adicionar ao carrinho
+  let carrinho = document.getElementById("lista-produtos"); //id da section q representa o carrinho
+  carrinho.innerHTML =
+    carrinho.innerHTML +
+    `<section class="carrinho__produtos__produto"> 
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`; //adicionar um novo na lista junto ao existente
+  //atualizar o valor total
 }
 
 function limpar() {
